2396af28714c69909a8d95957701242b2f3d3749
Jônata Marcelino <60443353+johny83br@users.noreply.github.com>
2026-04-20 04:58:05 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 function Home() {
   return (
-    <h1>Código em silêncio<br />
-Luzes piscam na noite<br />
-Mundo em conexão</h1>
-  );
+    <h1>
+      Código em silêncio
+      <br />
+      Luzes piscam na noite
+      <br />
+      Mundo em conexão
+    </h1>
+  )
 }
 
-export default Home;
+export default Home
