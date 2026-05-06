53d9c642f52347dadb3e6b67d591d167466ace57
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-05-06 13:31:40 -0300

    verificado se o ambiente é de produção

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(sql, params) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === 'production' ? true : false
   };
 
   const client = new Client(objQueryConfig);
